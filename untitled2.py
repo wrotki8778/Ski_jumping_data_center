293e6bba6f7d8d8c67b9b9cace909ed0349d290a
wrotki8778 <wrotki8778@gmail.com>
2021-02-16 16:10:49 +0100

    Stats parsing included (ver. 0.001)

--- untitled2.py
+++ untitled2.py
@@ -36,6 +36,29 @@ def parse_weather(comp):
                  and sum([x.count(word) for word in word_acc])]
     return tekst_lin
 
+def parse_stats(comp):
+    if not os.path.isfile(os.getcwd()+'//'+comp['id']+'.pdf'):
+        return [[comp['id'], '']]
+    print(comp.name)
+    parsed = parser.from_file(comp['id']+'.pdf')
+    tekst=parsed["content"]
+    tekst=tekst.lower()
+    tekst_lin=tekst.splitlines()
+    tekst_lin = [i for i in tekst_lin if i] 
+    word1 = 'weather information'
+    word2 = 'statistics'
+    try:
+        start = min([i for i,x in enumerate(tekst_lin) if x.count(word2)])
+    except ValueError:
+        return [[comp['id'], '']]
+    tekst_lin = tekst_lin[start:]
+    word_acc = ['1st round', 'trial round', '2nd round',
+                'training', 'qualification', 'final round']
+    tekst_lin = [[comp['id'], x] for x in tekst_lin 
+                 if sum(c.isdigit() for c in x) > 4
+                 and sum([x.count(word) for word in word_acc])]
+    return tekst_lin
+
 def process_weather_init(data,comps):
     fis_code = data[0]
     line = data[1]
@@ -127,8 +150,15 @@ u = process_weather_init(all_data[1027],actual_comps)
 all_error_data = [all_data[i] for i, x in enumerate(processed_data)
                   if x[5] == 'error']
 error_data = [x for x in processed_data if x[5]=='error']
-comp=actual_comps.iloc[10] 
 
+comp=actual_comps.iloc[750] 
+parsed = parser.from_file(comp['id']+'.pdf')
+tekst=parsed["content"]
+tekst=tekst.lower()
+tekst_lin=tekst.splitlines()
+tekst_lin = [i for i in tekst_lin if i] 
+stats_data = [parse_stats(comp) for i,comp in actual_comps.iterrows()]
+all_stats_data = [item for sublist in stats_data for item in sublist]
 
 
 
