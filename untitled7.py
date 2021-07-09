5978153b76fb1d74056e8c43a4d5e74deb00ffab
wrotki8778 <44114906+wrotki8778@users.noreply.github.com>
2021-07-09 22:25:30 +0200

    Standarization of the LSTM model

--- untitled7.py
+++ untitled7.py
@@ -12,6 +12,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
 from matplotlib.pyplot import plot, scatter
 import statistics as st
 import random
@@ -58,7 +59,7 @@ actual_results_lstm = actual_results_lstm.dropna()
 sc = StandardScaler()
 sc2 = StandardScaler()
 var_names = ['hill_size_x', 'cumm_rating', 'short_rating',
-             'wind', 'speed', 'gender']
+             'wind', 'speed', 'gender','codex_x']
 all_var_names = var_names + ['norm_dist']
 input_len = len(all_var_names)
 actual_results_lstm[var_names]\
@@ -108,12 +109,12 @@ testY = Variable(torch.Tensor(np.array(y[train_size:len(y)])))
 """
 
 class LSTM(nn.Module):
-    def __init__(self, input_size=input_len, hidden_layer_size=100, output_size=1, num_layers=1):
-        super().__init__()
+    def __init__(self, input_size=input_len, hidden_layer_size=50,
+                 output_size=1, num_layers=1):
+        super(LSTM, self).__init__()
         self.hidden_layer_size = hidden_layer_size
-
-        self.lstm = nn.LSTM(input_size, hidden_layer_size, num_layers)
-
+        self.lstm = nn.LSTMCell(input_size, hidden_layer_size, num_layers)
+        self.lstm_2 = nn.LSTMCell(hidden_layer_size, hidden_layer_size, num_layers)
         self.linear = nn.Linear(hidden_layer_size, output_size)
         self.dropout = nn.Dropout(p=0.2)
 
@@ -121,10 +122,24 @@ class LSTM(nn.Module):
                             torch.zeros(num_layers,1,self.hidden_layer_size))
 
     def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
-        predictions = self.dropout(lstm_out.view(len(input_seq), -1))
-        predictions = self.linear(predictions)
-        return predictions[-1]
+        output = []
+        h_t = torch.zeros(1, self.hidden_layer_size)
+        c_t = torch.zeros(1, self.hidden_layer_size)
+        h_t2 = torch.zeros(1, self.hidden_layer_size)
+        c_t2 = torch.zeros(1, self.hidden_layer_size)
+        error = 0
+        for input_t in input_seq:
+            input_to_network = torch.cat((input_t[:-1],
+                                          torch.Tensor([error])),0)
+            h_t, c_t = self.lstm(input_to_network.unsqueeze(0), (h_t, c_t))
+            h_t2, c_t2 = self.lstm_2(h_t, (h_t2, c_t2))
+            # predictions = self.dropout(lstm_out.view(len(input_seq), -1))
+            output += [self.linear(h_t2)]
+            act_dist = input_t[-1]
+            error = 0.9*error + 0.1*(act_dist - output[-1])
+        outputs = torch.cat(output, dim=1)
+        return sum(outputs)
+
 
 model = LSTM()
 loss_function = nn.MSELoss()
@@ -145,19 +160,19 @@ for epoch in range(epochs_total):
     model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
                          torch.zeros(1, 1, model.hidden_layer_size))
     x_pred = x[u][max(i-10,0):i+1].detach().clone()
-    x_pred[-1, -1] = 0
     y_pred = model(x_pred)
+    y_true = y[u][max(i-10,0):i+1].detach().clone()
     if(epoch == epochs_total):
         print(x_pred)
         print(epoch)
         print(y_pred)
-        print(y[u][i].data.numpy())
-        print(loss_function(y_pred, y[u][i]))
-    single_loss = loss_function(y_pred, y[u][i])
+        print(y_true)
+        print(loss_function(y_pred, y_true))
+    single_loss = loss_function(y_pred, y_true)
     single_loss.backward()
     losses = losses + [single_loss.item()]
     optimizer.step()
-    if epoch%5 == 2:
+    if epoch%50 == 2:
         print(f'epoch: {epoch:3} loss: {st.mean(losses):10.8f}')
 
 def moving_average(x, w):
@@ -167,11 +182,6 @@ plot(moving_average(np.log10(np.array(losses)),100))
 
 
 # Test the model
-def r_squared(data_1,data_2):
-    u = st.mean(data_2)
-    total_ssq = sum((data_2-u)**2)
-    model_ssq = sum((data_1-u)**2)
-    return(model_ssq/total_ssq)
 
 def predict_lstm(model, code):
     outputs_collect = []
@@ -180,30 +190,32 @@ def predict_lstm(model, code):
     filtered_dict = [u[0] for u in filtered_dict.iterrows()]
     ha_es = comp['hill_size_x'].item()
     for instance in filtered_dict:
-        for i in range(1,x[instance].shape[0]):
-            with torch.no_grad():
-                model.eval()
-                model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
-                                     torch.zeros(1, 1, model.hidden_layer_size))
-                x_pred = x[instance][:i+1].detach().clone()
-                x_pred[-1, -1] = 0
-                y_pred = model(x_pred)
-                y_pred = sc2.inverse_transform([y_pred.item()])
-                y_true = sc2.inverse_transform([y[instance][i].item()])
-                outputs_collect = outputs_collect \
-                    + [np.c_[ha_es*y_pred, ha_es*y_true]]
-    df = np.array(outputs_collect).squeeze()
-    f1 = plt.figure()
+        model.eval()
+        model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
+                             torch.zeros(1, 1, model.hidden_layer_size))
+        with torch.no_grad():
+            x_pred = x[instance].detach().clone()
+            #x_pred[-1, -1] = 0
+            y_pred = model(x_pred)
+            y_pred = sc2.inverse_transform(y_pred)
+            y_true = sc2.inverse_transform(y[instance])
+            outputs_collect = outputs_collect + [np.array([ha_es*(y_pred-y_true),ha_es*y_pred ,ha_es*y_true])]
+    return outputs_collect
+id_test = ['2017JP3817RL','2019JP3192RL','2018JP3068RL','2015JP3880RL']
+model.eval()
+results=[]
+for code in id_test:
+    results.append(predict_lstm(model, code))
+
+
+"""
+print(df)
     scatter(df[:,0],df[:,1])
+    X = np.linspace(np.amin(df),np.amax(df))
+    plot(X,X)
     plt.show()
     plt.close()
-    r_sq = r_squared(df[:,0],df[:,1])
+    r_sq = r2_score(df[:,1],df[:,0])
     print(code)
     print('The R squared is equal to: {}'.format(r_sq))
-    return outputs_collect
-id_test = ['2017JP3817RL','2019JP3192RL','2018JP3068RL']
-model.eval()
-for code in id_test:
-    results = predict_lstm(model, code)
-
-
+"""
